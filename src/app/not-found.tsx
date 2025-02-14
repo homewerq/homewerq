25c147c2e22e1136a01134b9ea0a078a79b0e191
feiyeur <fishyua@outlook.com>
2025-02-14 10:41:50 +0800

    ux: 添加404错误页

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="p-6 flex flex-col">
+      <h1 className="text-4xl">:(</h1>
+      <h2 className="text-2xl">404 Not Found</h2>
+    </div>
+  )
+}
